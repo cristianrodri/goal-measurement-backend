cade41342004c78d47d71b669f3ad089fe051fd7
Cristian Rodriguez <contacto.cristianrodriguez@gmail.com>
2022-07-24 15:55:24 -0300

    add sendgrid provider

---
+++ config/plugins.js
@@ -0,0 +1,15 @@
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: "sendgrid",
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: env("SENDGRID_EMAIL"),
+        defaultReplyTo: env("SENDGRID_EMAIL"),
+        testAddress: env("SENDGRID_EMAIL"),
+      },
+    },
+  },
+});
