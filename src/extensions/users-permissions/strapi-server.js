7e83fbed3a576ff84a640c84b8a5311497f19e16
Cristian Rodriguez <contacto.cristianrodriguez@gmail.com>
2022-08-04 01:40:39 -0300

    override auth register controller by changing email and username custom error message if one of them is taken by another user

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -1,11 +1,23 @@
 const {
   update
 } = require('@strapi/plugin-users-permissions/server/controllers/user')
+const {
+  validateRegisterBody
+} = require('@strapi/plugin-users-permissions/server/controllers/validation/auth')
 const { getService } = require('@strapi/plugin-users-permissions/server/utils')
 const utils = require('@strapi/utils')
 const crypto = require('crypto')
+const _ = require('lodash')
 
+const { sanitize } = utils
 const { ApplicationError, ValidationError } = utils.errors
+
+const sanitizeUser = (user, ctx) => {
+  const { auth } = ctx.state
+  const userSchema = strapi.getModel('plugin::users-permissions.user')
+
+  return sanitize.contentAPI.output(user, userSchema, { auth })
+}
 
 const isNotOwnUser = (ctxState, ctxParams) => {
   return ctxState.user.id !== +ctxParams.id
@@ -20,7 +32,111 @@ const deleteEmailToken = async userId => {
   })
 }
 
+// Verify if the email or username is already taken by another user
+const verifyExistingAccount = async (email, username, provider = null) => {
+  const addProvider = provider ? { provider } : {}
+
+  const conflictingUserEmailCount = await strapi
+    .query('plugin::users-permissions.user')
+    .count({
+      where: {
+        $or: [{ email: email.toLowerCase().trim() }, { email: username }],
+        ...addProvider
+      }
+    })
+
+  if (conflictingUserEmailCount > 0) {
+    throw new ApplicationError('Email is already taken')
+  }
+
+  const conflictingUsernameCount = await strapi
+    .query('plugin::users-permissions.user')
+    .count({
+      where: {
+        $or: [{ username: email.toLowerCase().trim() }, { username }],
+        ...addProvider
+      }
+    })
+
+  if (conflictingUsernameCount > 0) {
+    throw new ApplicationError('Username is already taken')
+  }
+}
+
 module.exports = plugin => {
+  // Update register controller
+  plugin.controllers.auth.register = async ctx => {
+    const pluginStore = await strapi.store({
+      type: 'plugin',
+      name: 'users-permissions'
+    })
+
+    const settings = await pluginStore.get({ key: 'advanced' })
+
+    if (!settings.allow_register) {
+      throw new ApplicationError('Register action is currently disabled')
+    }
+
+    const params = {
+      ..._.omit(ctx.request.body, [
+        'confirmed',
+        'blocked',
+        'confirmationToken',
+        'resetPasswordToken',
+        'provider'
+      ]),
+      provider: 'local'
+    }
+
+    await validateRegisterBody(params)
+
+    const role = await strapi
+      .query('plugin::users-permissions.role')
+      .findOne({ where: { type: settings.default_role } })
+
+    if (!role) {
+      throw new ApplicationError('Impossible to find the default role')
+    }
+
+    const { email, username, provider } = params
+
+    await verifyExistingAccount(email, username, provider)
+
+    if (settings.unique_email) {
+      await verifyExistingAccount(email, username)
+    }
+
+    let newUser = {
+      ...params,
+      role: role.id,
+      email: email.toLowerCase(),
+      username,
+      confirmed: !settings.email_confirmation
+    }
+
+    const user = await getService('user').add(newUser)
+
+    const sanitizedUser = await sanitizeUser(user, ctx)
+    sanitize
+
+    if (settings.email_confirmation) {
+      try {
+        await getService('user').sendConfirmationEmail(sanitizedUser)
+      } catch (err) {
+        throw new ApplicationError(err.message)
+      }
+
+      return ctx.send({ user: sanitizedUser })
+    }
+
+    const jwt = getService('jwt').issue(_.pick(user, ['id']))
+
+    return ctx.send({
+      jwt,
+      user: sanitizedUser
+    })
+  }
+
   // Update user controller
   plugin.controllers.user.update = async ctx => {
     // Verify if the params id owns to the authenticated user
