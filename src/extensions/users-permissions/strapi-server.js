401ecc454ba4bf8f71ba9b2d56f9485bed9bd75f
Cristian Rodriguez <contacto.cristianrodriguez@gmail.com>
2022-08-06 04:55:56 -0300

    update auth callback controller

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -2,7 +2,8 @@ const {
   update
 } = require('@strapi/plugin-users-permissions/server/controllers/user')
 const {
-  validateRegisterBody
+  validateRegisterBody,
+  validateCallbackBody
 } = require('@strapi/plugin-users-permissions/server/controllers/validation/auth')
 const { getService } = require('@strapi/plugin-users-permissions/server/utils')
 const utils = require('@strapi/utils')
@@ -64,7 +65,7 @@ const verifyExistingAccount = async (email, username, provider = null) => {
 }
 
 module.exports = plugin => {
-  // Update register controller
+  // Update auth register controller
   plugin.controllers.auth.register = async ctx => {
     const pluginStore = await strapi.store({
       type: 'plugin',
@@ -135,6 +136,84 @@ module.exports = plugin => {
       jwt,
       user: sanitizedUser
     })
+  }
+
+  // Update auth callback controller
+  plugin.controllers.auth.callback = async ctx => {
+    const provider = ctx.params.provider || 'local'
+    const params = ctx.request.body
+
+    const store = strapi.store({ type: 'plugin', name: 'users-permissions' })
+    const grantSettings = await store.get({ key: 'grant' })
+
+    const grantProvider = provider === 'local' ? 'email' : provider
+
+    if (!_.get(grantSettings, [grantProvider, 'enabled'])) {
+      throw new ApplicationError('This provider is disabled')
+    }
+
+    if (provider === 'local') {
+      await validateCallbackBody(params)
+
+      const { identifier } = params
+
+      // Check if the user exists.
+      const user = await strapi
+        .query('plugin::users-permissions.user')
+        .findOne({
+          where: {
+            provider,
+            $or: [{ email: identifier.toLowerCase() }, { username: identifier }]
+          }
+        })
+
+      if (!user) {
+        throw new ValidationError('Invalid identifier')
+      }
+
+      if (!user.password) {
+        throw new ValidationError('Invalid identifier or password')
+      }
+
+      const validPassword = await getService('user').validatePassword(
+        params.password,
+        user.password
+      )
+
+      if (!validPassword) {
+        throw new ValidationError('Invalid password')
+      }
+
+      const advancedSettings = await store.get({ key: 'advanced' })
+      const requiresConfirmation = _.get(advancedSettings, 'email_confirmation')
+
+      if (requiresConfirmation && user.confirmed !== true) {
+        throw new ApplicationError('Your account email is not confirmed')
+      }
+
+      if (user.blocked === true) {
+        throw new ApplicationError(
+          'Your account has been blocked by an administrator'
+        )
+      }
+
+      return ctx.send({
+        jwt: getService('jwt').issue({ id: user.id }),
+        user: await sanitizeUser(user, ctx)
+      })
+    }
+
+    // Connect the user with the third-party provider.
+    try {
+      const user = await getService('providers').connect(provider, ctx.query)
+
+      return ctx.send({
+        jwt: getService('jwt').issue({ id: user.id }),
+        user: await sanitizeUser(user, ctx)
+      })
+    } catch (error) {
+      throw new ApplicationError(error.message)
+    }
   }
 
   // Update user controller
