6bdc7a918a256e36fc99c22b3baa9305e0dcf0e1
Cristian Rodriguez <contacto.cristianrodriguez@gmail.com>
2022-08-14 16:31:02 -0300

    add custom create controller into goal api

--- src/api/goal/controllers/goal.js
+++ src/api/goal/controllers/goal.js
@@ -6,4 +6,35 @@
 
 const { createCoreController } = require('@strapi/strapi').factories
 
-module.exports = createCoreController('api::goal.goal')
+module.exports = createCoreController('api::goal.goal', ({ strapi }) => ({
+  async create(ctx) {
+    ctx.request.body = { ...ctx.request.body, user: ctx.state.user }
+    const goalActivities = ctx.request.body.goalActivities
+
+    delete ctx.request.body.goalActivities
+
+    const goal = await strapi.entityService.create('api::goal.goal', {
+      data: {
+        ...ctx.request.body
+      }
+    })
+
+    const goalActivitiesEntities = await Promise.all(
+      goalActivities.map(async goalActivity => {
+        goalActivity.user = ctx.state.user
+        goalActivity.goal = goal
+        const entity = await strapi.entityService.create(
+          'api::goal-activity.goal-activity',
+          {
+            data: {
+              ...goalActivity
+            }
+          }
+        )
+        return entity
+      })
+    )
+
+    ctx.send({ ...goal, goalActivities: goalActivitiesEntities })
+  }
+}))
