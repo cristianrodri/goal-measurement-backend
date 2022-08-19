bf08da62e632195d3d54353282b8004bcd8d4da1
Cristian Rodriguez <contacto.cristianrodriguez@gmail.com>
2022-08-19 11:14:27 -0300

    override create controller into goal activity api

--- src/api/goal-activity/controllers/goal-activity.js
+++ src/api/goal-activity/controllers/goal-activity.js
@@ -5,5 +5,37 @@
  */
 
 const { createCoreController } = require('@strapi/strapi').factories
+const { avoidUpdatingSchema } = require('@utils/utils')
 
-module.exports = createCoreController('api::goal-activity.goal-activity')
+module.exports = createCoreController(
+  'api::goal-activity.goal-activity',
+  ({ strapi }) => ({
+    async create(ctx) {
+      avoidUpdatingSchema(ctx)
+
+      const userGoal = await strapi.entityService.findMany('api::goal.goal', {
+        filters: {
+          id: ctx.request.body?.goal ?? null,
+          user: ctx.state.user
+        }
+      })
+
+      if (userGoal.length === 0) {
+        return ctx.notFound('Goal id not found')
+      }
+
+      // Add the relation with the authenticated user
+      ctx.request.body.user = ctx.state.user
+      const entity = await strapi.entityService.create(
+        'api::goal-activity.goal-activity',
+        {
+          data: {
+            ...ctx.request.body
+          }
+        }
+      )
+
+      ctx.body = await this.sanitizeOutput(entity, ctx)
+    }
+  })
+)
