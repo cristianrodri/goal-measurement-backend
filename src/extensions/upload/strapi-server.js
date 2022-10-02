25e05b99105207af3f7d060b1485f2de16834a5d
Cristian Rodriguez <contacto.cristianrodriguez@gmail.com>
2022-10-02 19:41:42 -0300

    user avatar can only update its own avatar and file must not exceed 1MB

--- src/extensions/upload/strapi-server.js
+++ src/extensions/upload/strapi-server.js
@@ -1,7 +1,19 @@
 const _ = require('lodash')
+const { getService } = require('@strapi/plugin-users-permissions/server/utils')
 const utils = require('@strapi/utils')
 
 const { ValidationError } = utils.errors
+
+const hasUserTheAvatarId = async (ctx, avatarId) => {
+  const user = await getService('user').fetch(ctx.state.user.id, {
+    populate: {
+      avatar: true
+    }
+  })
+
+  if (user.avatar.id !== +avatarId)
+    throw new ValidationError('Query id is not related with the user avatar id')
+}
 
 module.exports = plugin => {
   plugin.controllers['content-api'].upload = async ctx => {
@@ -10,6 +22,14 @@ module.exports = plugin => {
       request: { files: { files } = {} }
     } = ctx
 
+    if (id) {
+      await hasUserTheAvatarId(ctx, id)
+    }
+
+    if (files.size > 1_000_000)
+      throw new ValidationError('File size exceeded 1MB')
+
+    // Relate the refId with the authenticated user id
     ctx.request.body.refId = ctx.state.user.id
 
     if (id && (_.isEmpty(files) || files.size === 0)) {
