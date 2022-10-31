462be9878128c4ef771d00443fe17edecb225571
Cristian <contacto.cristianrodriguez@gmail.com>
2022-10-31 11:04:01 -0300

    avoid creating reward with the same description in the same type

--- src/utils/reward.js
+++ src/utils/reward.js
@@ -1,4 +1,7 @@
+const utils = require('@strapi/utils')
 const { REWARD_API_NAME } = require('./api_names')
+
+const { ApplicationError } = utils.errors
 
 const createReward = (ctx, reward, type) =>
   strapi.entityService
@@ -12,6 +15,20 @@ const createReward = (ctx, reward, type) =>
     .then(res => res)
 
 const createManyRewards = async (ctx, rewards, type) => {
+  // Find all user rewards belong to the given type
+  const typeRewards = await findRewards(ctx.state.user, type)
+
+  // Verify if some given reward with the given type already exists in the database
+  const repeatedReward = rewards.find(reward =>
+    typeRewards.some(r => r.description === reward)
+  )
+
+  // If some repetead reward is found, throw an error
+  if (repeatedReward)
+    throw new ApplicationError(
+      `${repeatedReward} already exists in the ${type} type`
+    )
+
   const rewardEntities = await Promise.all(
     rewards.map(async reward => {
       const entity = await createReward(ctx, reward, type)
@@ -32,6 +49,16 @@ const findOneReward = ctx =>
       }
     })
     .then(res => res[0])
+
+const findRewards = async (user, type) =>
+  strapi.entityService
+    .findMany(REWARD_API_NAME, {
+      filters: {
+        user,
+        type
+      }
+    })
+    .then(res => res)
 
 const findManyRewards = ctx =>
   strapi.entityService
